d51555c68c1717ad3cf345a6958d93c7ae63affc
gwendolineNK <kuidjaugwendoline06@gmail.com>
2026-07-08 21:01:14 +0200

    j'ai change spiel de exam2 parce que je me suis rendue compte de comment fonctionne testrunner Minesucher et jai ajuste les uniquement les return de la methode auswertung

--- Spiel.java
+++ Spiel.java
@@ -9,6 +9,8 @@ public class Spiel {
 
     private static Scanner sc = new Scanner(System.in);
     private static Spielfeld feld;
+    private static int zeile;
+    private static int spalte;
 
     public static void main(String[] args) {
         int wahl = menu();
@@ -23,7 +25,7 @@ public class Spiel {
      * TODO 14 (mittel): Zeigt das Menue so lange an, bis 1 oder 2 eingegeben wird,
      * und gibt diese Zahl zurueck.
      * Menue-Text (genau so):
-     *     Minesucher
+     *     Herzlich Willkommen zur Schatzsuche
      *     1 - Spielen
      *     2 - Programm beenden
      * Hinweis: do-while-Schleife + checkEingabeMenu(...).
@@ -32,60 +34,92 @@ public class Spiel {
         // TODO 14
     	int eingabe;
     	do {
-    		System.out.println("Minesucher");
+    		System.out.println("Herzlich Willkommen zur Schatzsuche");
     		System.out.println("1 - Spielen");
-    		System.out.println("2 - Programm beendet.");
+    		System.out.println("2 - Programm beenden");
     		eingabe = sc.nextInt();
     	}while(!checkEingabeMenu(eingabe));
         return eingabe;
+    	
     }
 
     /** TODO 13 (leicht): true, wenn eingabe 1 oder 2 ist, sonst false. */
     public static boolean checkEingabeMenu(int eingabe) {
         // TODO 13
         return eingabe == 1 || eingabe == 2;
+    	
+    }
+
+    /**
+     * TODO 15 (mittel): Gibt true zurueck, wenn noch NICHT alle Schaetze gefunden wurden.
+     * Es gibt anzahlSchaetze Schaetze. Gefunden = Summe der Punkte beider Spieler.
+     * Hinweis: return (Summe der Punkte) < anzahlSchaetze;
+     */
+    public static boolean weiterspielen(Spieler s1, Spieler s2, int anzahlSchaetze) {
+        // TODO 15
+    	
+        return (s1.getPunkte() + s2.getPunkte()) < anzahlSchaetze;
+    	
+    }
+
+    /**
+     * TODO 16 (mittel): Ergebnis-Satz.
+     * - Hat s1 mehr Punkte:  "<Name1> hat gewonnen!"
+     * - Hat s2 mehr Punkte:  "<Name2> hat gewonnen!"
+     * - Gleichstand:         "Unentschieden."
+     */
+    public static String auswertung(Spieler s1, Spieler s2) {
+        // TODO 16
+    	if(s1.getPunkte() > s2.getPunkte()) {
+    		return s1.getName() + " hat gewonnen!";
+    	}
+    	else if(s2.getPunkte() > s1.getPunkte()) {
+    		return s2.getName() + " hat gewonnen!";
+    	}
+    	else if(s2.getPunkte() == s1.getPunkte()) {
+    		return "Unentschieden.";
+    	}
+        return "";
     }
 
     /** Ein komplettes Spiel. (gegeben - nutzt deine Methoden) */
     private static void spielen() {
-        System.out.println("Gib deinen Namen ein:");
-        Spieler sp = new Spieler(sc.next());
+        System.out.println("Gib den Namen von Spieler 1 ein:");
+        Spieler s1 = new Spieler(sc.next(), 1);
+        System.out.println("Gib den Namen von Spieler 2 ein:");
+        Spieler s2 = new Spieler(sc.next(), 2);
 
         System.out.println("Gib die Groesse des Spielfelds ein:");
         int n = sc.nextInt();
-        System.out.println("Gib die Anzahl der Minen ein:");
-        int m = sc.nextInt();
-        feld = new Spielfeld(n, m);
+        feld = new Spielfeld(n);
+        int anzahlSchaetze = n;   // n Schaetze auf einem n x n Feld
 
-        while (true) {
+        Spieler aktuell = s1;
+        while (weiterspielen(s1, s2, anzahlSchaetze)) {
+            System.out.println(aktuell.getName() + " ist an der Reihe.");
             System.out.println(feld);
 
-            int[] stelle = leseGueltigeStelle(sp);
-            int z = stelle[0];
-            int s = stelle[1];
-            feld.aufdecken(z, s);
+            eingabe(aktuell);   // setzt zeile und spalte
+            String ergebnis = feld.deckeAuf(zeile, spalte, aktuell);
+            System.out.println(ergebnis);
 
-            if (feld.istMine(z, s)) {
-                System.out.println(feld);
-                System.out.println("BOOM! Eine Mine. " + sp.getName() + " hat verloren.");
-                return;
+            if (ergebnis.equals(Spielfeld.TREFFER)) {
+                aktuell.punktDazu();          // Punkt und nochmal dran
+            } else {
+                aktuell = (aktuell == s1) ? s2 : s1;   // Spielerwechsel
             }
 
-            sp.punktDazu();
-
-            if (feld.alleSicherenAufgedeckt()) {
-                System.out.println(feld);
-                System.out.println(sp.getName() + " hat gewonnen! Punkte: " + sp.getPunkte());
-                return;
-            }
+            zeigeSpielstand(s1, s2);
         }
+
+        System.out.println(auswertung(s1, s2));
     }
 
     /**
-     * Fragt so lange nach Zeile und Spalte, bis eine gueltige Eingabe erfolgt,
-     * und gibt sie als {zeile, spalte} zurueck. (gegeben - nicht aendern)
+     * Liest so lange Zeile und Spalte ein, bis eine gueltige Eingabe erfolgt,
+     * und setzt die Attribute zeile und spalte. (gegeben - nicht aendern)
      */
-    private static int[] leseGueltigeStelle(Spieler sp) {
+    private static void eingabe(Spieler sp) {
         int z, s;
         do {
             System.out.println(sp.getName() + ", gib eine Zeile an:");
@@ -93,6 +127,14 @@ public class Spiel {
             System.out.println(sp.getName() + ", gib eine Spalte an:");
             s = sc.nextInt();
         } while (!feld.gueltigeEingabe(z, s));
-        return new int[] { z, s };
+        zeile = z;
+        spalte = s;
+    }
+
+    /** Gibt den aktuellen Spielstand aus. (gegeben - nicht aendern) */
+    private static void zeigeSpielstand(Spieler s1, Spieler s2) {
+        System.out.println("Der aktuelle Spielstand lautet:");
+        System.out.println(s1.getName() + ": " + s1.getPunkte() + " Punkte");
+        System.out.println(s2.getName() + ": " + s2.getPunkte() + " Punkte");
     }
 }
